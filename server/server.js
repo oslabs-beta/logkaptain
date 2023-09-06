78dc75a347912e0f330b66fa35d5a4c334aab68b
Gabriela Passentino <gpassentino@gmail.com>
2023-09-06 14:40:52 -0700

    finished db and model. Still working on server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,32 @@
 // server here
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+const apiRouter = require('./routes/routes');
+
+const PORT = 3000; //QUESTION Not sure if this is the port we will be listening on
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Global error handler
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      //this is console logged for developers
+      log: 'Express error handler caught unknown middleware error',
+      //status code sent to client
+      status: 400,
+      //response that client will see in inspect/errors
+      message: { err: 'An error occurred' },
+    };
+    //global err handler will create a new err combining midlleware err and globar err
+    //the later arg will override the previous arg.
+    //anything in err will override defaultErr
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+  
+  app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
