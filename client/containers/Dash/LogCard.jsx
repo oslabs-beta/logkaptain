bedf6fd0808bd945c2be68872cd7ffa0dee71108
sharmarkeGit <shar.alternatif@gmail.com>
2023-09-11 10:08:42 -0400

    connected

--- client/containers/Dash/LogCard.jsx
+++ client/containers/Dash/LogCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../../stylesheets/dashboard.css'
 import LogTable from './LogTable'
+import { useState } from 'react';
 
 
 
@@ -8,34 +9,37 @@ import LogTable from './LogTable'
 const LogCard = () => {
 
 // ARRAY FOR ALL LOG DATA //
-let logs = [];
+const [logs, setLogs] = useState([]);
+
 
 // GET ALL LOGS CURRENTLY IN THE DB //
 const gatherLogs = async () => {
-    logs = [];
     try {
-        const response = await fetch('/api/logs', {
+        const response = await fetch('http://localhost:3000/api/logs', {
             headers: { 'Content-Type': 'application/json' },
         });
         const data = await response.json();
-        for (const log of data) {
-           logs.push( < LogTable props={log} /> )
+        const components = [];
+        for (const log of Object.keys(data)) {
+            //console.log(log)
+            components.push( <LogTable key={log} name={log} log={data[log]}/> )
         }
-        console.log('LOGS:', logs)
+        setLogs(components);
+       // console.log('LOGS:', logs)
+        //console.log('data',data['bb-demo-69f7d75fcf-f4zpv'])
     } catch (error) {
         console.log(error);
     }
 }
 
 // CHECK FOR NEW LOGS EVERY 2 SECOND (FOR NOW) //
-document.addEventListener('DOMContentLoaded', () => {
-    const retrievelogsBTN = document.querySelector('#retrievelogs')
-    retrievelogsBTN.addEventListener('click', () => {
-        gatherLogs();
-        setInterval(gatherLogs, 2000);
-    })
-    
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//     const retrievelogsBTN = document.querySelector('#retrievelogs')
+//     retrievelogsBTN.addEventListener('click', () => {
+//         gatherLogs();
+//         //setInterval(gatherLogs, 2000);
+//     })
+// })
 
 
 
@@ -44,10 +48,10 @@ document.addEventListener('DOMContentLoaded', () => {
         <div className="logcard">
             <div className="cardheader">
                 <h2 className="listheader">Log Data</h2>
-                <button className="connectpod">Retrieve Logs</button>
+                <button className="connectpod" onClick={gatherLogs}>Retrieve Logs</button>
                 <button className="connectpod" id='retrievelogs'>CONNECT POD</button>
             </div>
-            <LogTable />
+            {logs}
             {/* {logs} ARRAY OF LOG INFO TO BE DISPLAYED */}
         </div>
     );
