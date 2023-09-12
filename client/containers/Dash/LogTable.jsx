c09ac4dab7f0b5dc6156393a20e7256903f919f0
sharmarkeGit <shar.alternatif@gmail.com>
2023-09-12 14:44:03 -0400

    logs sanitized

--- client/containers/Dash/LogTable.jsx
+++ client/containers/Dash/LogTable.jsx
@@ -5,14 +5,19 @@ import '../../stylesheets/dashboard.css'
 
 // LOG DATA PASSED DOWN INTO PROPS //
 const LogTable = (props) => {
+    const {date, name, log} = props;
 
 console.log('PROPS', props);
 
     return (
-        <div>
-        <div className="outerlogcontainer">
+        <>
+        {/* <div className="outerlogcontainer"> */}
             <div className="innerlogcontainer">
-            <table>
+                {/* <h2>{name}</h2> */}
+                <div>{date}</div>
+                <div>{name}</div>
+                <div>{log}</div>
+            {/* <table>
                 <thead>
                 <tr>
                     <th>DATE</th>
@@ -24,160 +29,16 @@ console.log('PROPS', props);
                     <tr>
                     <td>September 9th 2023</td>
                     {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
+                    {/* <td>OSP1 Rules</td> */}
                     {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
+                    {/* <td>POD1</td> */}
                     {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                    <tr>
-                    <td>September 9th 2023</td>
-                    {/* <td>{props.timestamp}</td> date info from BE */}
-                    <td>OSP1 Rules</td>
-                    {/* <td>{props.log}</td> log info from BE */}
-                    <td>POD1</td>
-                    {/* <td>{props.id}</td> ID info from BE */}
-                    </tr>
-                </tbody>
-            </table>
-            </div>
+                    {/* </tr> */}
+                {/* </tbody> */}
+            {/* </table> */} 
+            {/* </div> */}
         </div>
-        </div>
+        </>
     );
 };
 
