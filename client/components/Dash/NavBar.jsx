116c550239d1ef0db6d1cb6d25a81eecadab0107
dragblas <chrisle96@yahoo.com>
2023-09-21 17:30:08 -0400

    removed local host dependencies on navbar

--- client/components/Dash/NavBar.jsx
+++ client/components/Dash/NavBar.jsx
@@ -8,10 +8,10 @@ const NavBar = () => {
 
         <ul className="navlist">
           <img src="../../../public/logo.png" id="pagelogo" ></img> 
-          <li className="dashnav"><a href="http://localhost:5173/dashboard">Dashboard</a></li>
-          <li className="teamnav"><a href="http://localhost:5173/dashboard">Team</a></li>
-          <li className="settingnav"><a href="http://localhost:5173/dashboard">Settings</a></li>
-          <li className="logoutnav"><a href="http://localhost:5173/login">Logout</a></li>
+          <li className="dashnav"><a href="/dashboard">Dashboard</a></li>
+          <li className="teamnav"><a href="/dashboard">Team</a></li>
+          <li className="settingnav"><a href="/dashboard">Settings</a></li>
+          <li className="logoutnav"><a href="/login">Logout</a></li>
         </ul>
       </div>
     </div>
