bab1189ac75f1261c9c2b51eafa517de4cc86a51
dragblas <chrisle96@yahoo.com>
2023-09-21 16:36:06 -0400

    added port options for heroku

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const routes = require('./routes/user');
 
 const apiRouter = require('./routes/api');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 //app.use(cors());
