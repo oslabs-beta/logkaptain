1a0fb1c9bfa57bdad364c2faf417dbe24540b679
dragblas <chrisle96@yahoo.com>
2023-09-22 15:16:36 -0400

    added handleclick function for navbar

--- client/components/Dash/NavBar.jsx
+++ client/components/Dash/NavBar.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import '../../stylesheets/dashboard.css';
 import '../../assets/logo.png'; 
-import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
   const navigate = useNavigate(); 
+
+  const handleClick = (route) => {
+    navigate(route);
+  }
   
   return (
     <div className="navbar">
       <div className="navbox">
         <ul className="navlist">
           <img src="logo.png" id="pagelogo" ></img> 
-          <li className="dashnav"><a href={navigate("/dashboard")}>Dashboard</a></li>
-          <li className="teamnav"><a href={navigate("/dashboard")}>Team</a></li>
-          <li className="settingnav"><a href={navigate("/dashboard")}>Settings</a></li>
-          <li className="logoutnav"><a href={navigate("/login")}>Logout</a></li>
+          <li className="dashnav" onClick={() => handleClick('/dashboard')}>Dashboard</li>
+          <li className="teamnav" onClick={() => handleClick('/dashboard')}>Team</li>
+          <li className="settingnav" onClick={() => handleClick('/dashboard')}>Settings</li>
+          <li className="logoutnav" onClick={() => handleClick('/dashboard')}>Logout</li>
         </ul>
       </div>
     </div>
