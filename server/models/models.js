e8c229fdd855edb2630184bd40b8c76c8588d993
sharmarkeGit <shar.alternatif@gmail.com>
2023-10-01 14:00:42 -0400

    refactored model with env variables

--- server/models/models.js
+++ server/models/models.js
@@ -1,8 +1,10 @@
 const { Pool } = require('pg'); //Pool class is used to manage connections to the database
 const { dbConnection } = require('../connections/db'); //Imports the db connection
+const config = require('../config')
 
 const pool = new Pool({
-  connectionString: dbConnection,
+  //connectionString: dbConnection,
+  connectionString: config.dbConnection,
 }); //a new Pool instance
 
 module.exports = {
