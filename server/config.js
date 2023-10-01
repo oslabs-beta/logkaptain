dd5c82e356d65f63e9c5dcf27a27d8d07042928d
sharmarkeGit <shar.alternatif@gmail.com>
2023-10-01 16:52:14 -0400

    created a config module forthe env variables

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    dbConnection: process.env.dbConnection,
+    apiTokenJira: process.env.apiTokenJira,
+    jiraUsername: process.env.jiraUsername,
+    jiraApiUrl: process.env.jiraApiUrl
+}
